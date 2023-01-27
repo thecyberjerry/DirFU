686c38387721a77b918b8c05aef671f92f4241ee
Himanshu <72544756+thecyberjerry@users.noreply.github.com>
2023-01-27 12:58:56 +0530

    Update dirfu.py

--- dirfu.py
+++ dirfu.py
@@ -1,12 +1,8 @@
 import requests
-import threading
 import argparse
 import sys
-from colorama import Fore
 import time
-import pyfiglet
-banner = pyfiglet.figlet_format("DirFU")
-print(banner)
+from colorama import Fore
 
 startTime= time.time()
 def dir(z,y):
@@ -18,7 +14,6 @@ def dir(z,y):
     for item in y:
         global url
         url = f'https://{z}/{item}'
-
         try:
             var4 = requests.get(url, timeout=10)
             if var4.status_code == 200:
@@ -43,17 +38,53 @@ def dir(z,y):
     with open('activeDirectories.txt', 'w') as active:
         active.write(var7)
 
+def post_dir(z,y):
+    with open('readme.md', 'r') as file:
+        var = file.read()
+        print(var)
+    print('\n====Started====')
+    var5 = {200: []}
+    for item in y:
+        global url
+        url = f'https://{z}/{item}'
+        try:
+            var4 = requests.post(url, timeout=10)
+            if var4.status_code == 200:
+                var5[200] += [url]
+                print('\n', url, Fore.BLUE + f'\n Status Code: {var4.status_code}', Fore.RESET)
+            else:
+                print('\n', url, Fore.YELLOW + f'\n Status Code: {var4.status_code}', Fore.RESET)
+
+        except requests.ConnectionError:
+            pass
+        except KeyboardInterrupt as e:
+            e = '\n KeyBoard Interrupt detected... Exiting!!'
+            print(e)
+            print("\n""====Finished!!====",
+                  end=("\n\nExecution time took: " + str(time.time() - startTime) + " seconds"))
+            var7 = '\n'.join(var5[200])
+            with open('activeDirectories.txt', 'w') as active:
+                active.write(var7)
+            sys.exit()
+    print("\n""====Finished!!====",end=("\n\nExecution time took: "+str(time.time()-startTime)+" seconds"))
+    var7 = '\n'.join(var5[200])
+    with open('activeDirectories.txt', 'w') as active:
+        active.write(var7)
 parser=argparse.ArgumentParser(
     usage= '''dirfu.py -d example.com -f /path/to/file.txt''',
     description='''DirFU a Directory Bruteforcer for beginners''',
     epilog="""Developer: @instagram.com/thecyberjerry""")
 parser.add_argument('-d','--domain',required=True,help='-d example.com')
 parser.add_argument('-f','--file',required=True, help='-f /path/to/file/file.txt')
+parser.add_argument('-p','--POST',help = '-p POST')
 args=parser.parse_args()
 Target = vars(args)
 with open(Target.get('file'),'r') as file:
     var2 = file.read()
     x = var2.splitlines()
-thread = threading.Thread(target=dir(Target.get('domain'),x))
-thread.start()
 
+if Target.get('POST') == 'POST':
+    post_dir(Target.get('domain'),x)
+
+else:
+    dir(Target.get('domain'),x)
