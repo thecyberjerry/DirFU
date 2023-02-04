7ce5d1d523d1b88e8deeb5bef3e7b1bd9a5f7d71
Himanshu <72544756+thecyberjerry@users.noreply.github.com>
2023-02-04 21:44:07 +0530

    Update dirfu.py

--- dirfu.py
+++ dirfu.py
@@ -1,90 +1,63 @@
-import requests
-import argparse
 import sys
+import requests
+import concurrent.futures
+import argparse
+import pyfiglet
 import time
 from colorama import Fore
 
 startTime= time.time()
-def dir(z,y):
-    with open('readme.md', 'r') as file:
-        var = file.read()
-        print(var)
-    print('\n====Started====')
-    var5 = {200: []}
-    for item in y:
-        global url
-        url = f'https://{z}/{item}'
-        try:
-            var4 = requests.get(url, timeout=10)
-            if var4.status_code == 200:
-                var5[200] += [url]
-                print('\n', url, Fore.BLUE + f'\n Status Code: {var4.status_code}', Fore.RESET)
-            else:
-                print('\n', url, Fore.YELLOW + f'\n Status Code: {var4.status_code}', Fore.RESET)
+banner = pyfiglet.figlet_format("DirFU  ")
+print(banner)
 
-        except requests.ConnectionError:
-            pass
-        except KeyboardInterrupt as e:
-            e = '\n KeyBoard Interrupt detected... Exiting!!'
-            print(e)
-            print("\n""====Finished!!====",
-                  end=("\n\nExecution time took: " + str(time.time() - startTime) + " seconds"))
-            var7 = '\n'.join(var5[200])
-            with open('activeDirectories.txt', 'w') as active:
-                active.write(var7)
-            sys.exit()
-    print("\n""====Finished!!====",end=("\n\nExecution time took: "+str(time.time()-startTime)+" seconds"))
-    var7 = '\n'.join(var5[200])
-    with open('activeDirectories.txt', 'w') as active:
-        active.write(var7)
-
-def post_dir(z,y):
-    with open('readme.md', 'r') as file:
-        var = file.read()
-        print(var)
-    print('\n====Started====')
-    var5 = {200: []}
-    for item in y:
-        global url
-        url = f'https://{z}/{item}'
-        try:
-            var4 = requests.post(url, timeout=10)
-            if var4.status_code == 200:
-                var5[200] += [url]
-                print('\n', url, Fore.BLUE + f'\n Status Code: {var4.status_code}', Fore.RESET)
-            else:
-                print('\n', url, Fore.YELLOW + f'\n Status Code: {var4.status_code}', Fore.RESET)
-
-        except requests.ConnectionError:
-            pass
-        except KeyboardInterrupt as e:
-            e = '\n KeyBoard Interrupt detected... Exiting!!'
-            print(e)
-            print("\n""====Finished!!====",
-                  end=("\n\nExecution time took: " + str(time.time() - startTime) + " seconds"))
-            var7 = '\n'.join(var5[200])
-            with open('activeDirectories.txt', 'w') as active:
-                active.write(var7)
-            sys.exit()
-    print("\n""====Finished!!====",end=("\n\nExecution time took: "+str(time.time()-startTime)+" seconds"))
-    var7 = '\n'.join(var5[200])
-    with open('activeDirectories.txt', 'w') as active:
-        active.write(var7)
 parser=argparse.ArgumentParser(
     usage= '''dirfu.py -d example.com -f /path/to/file.txt''',
     description='''DirFU a Directory Bruteforcer for beginners''',
     epilog="""Developer: @instagram.com/thecyberjerry""")
 parser.add_argument('-d','--domain',required=True,help='-d example.com')
 parser.add_argument('-f','--file',required=True, help='-f /path/to/file/file.txt')
-parser.add_argument('-p','--POST',help = '-p POST')
 args=parser.parse_args()
 Target = vars(args)
+url = Target.get('domain')
+urls = []
+
 with open(Target.get('file'),'r') as file:
-    var2 = file.read()
-    x = var2.splitlines()
+    var = file.read().splitlines()
 
-if Target.get('POST') == 'POST':
-    post_dir(Target.get('domain'),x)
+for item in var:
+    var2 = f'https://{url}/{item}'
+    urls.append(var2)
+dict = {200:[]}
+def main(url):
+    resp = requests.get(url)
+    v = f'\n{url} {Fore.YELLOW}Code: {resp.status_code}{Fore.RESET}'
+    if resp.status_code == 200:
+        dict[200] += [url]
+    return v
 
-else:
-    dir(Target.get('domain'),x)
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = []
+    try:
+        with open('readme.md', 'r') as file:
+            var = file.read()
+            print(var)
+        print('\t\t=========STARTED=========\n')
+        for u in urls:
+            futures.append(executor.submit(main, url=u))
+        for future in concurrent.futures.as_completed(futures):
+            print(future.result())
+    except requests.ConnectionError:
+        pass
+    except KeyboardInterrupt as e:
+        e = f'\n{Fore.WHITE}Keyboard Interrupt Detected Exiting!!\n\nResult will be saved in activeDirectory.txt File{Fore.RESET}'
+        print(e)
+        print("\n\t\t""=========Finished!!=========", end=(f"\n\n{Fore.YELLOW}Execution time took: " + str(time.time() - startTime) + f" seconds{Fore.RESET}"))
+        var7 = '\n'.join(dict[200])
+        with open('activeDirectories.txt', 'w') as active:
+            active.write(var7)
+        sys.exit()
+var7 = '\n'.join(dict[200])
+with open('activeDirectories.txt', 'w') as active:
+    active.write(var7)
+    print(f'\n{Fore.WHITE}Result will be saved in activeDirectory.txt File{Fore.RESET}')
+    print("\n\t\t""=========Finished!!=========", end=(f"\n\n{Fore.YELLOW}Execution time took: " + str(time.time() - startTime) + f" seconds{Fore.RESET}"))
